9570b928670bebb3c40971df4db9bf65b00756bd
Brian Goff <cpuguy83@gmail.com>
2013-06-12 21:52:59 -0400

    Remove adding icon to add-on since this can just be handled manually and is causing issues when no add-on is there.

--- vendor/assets/javascripts/bootstrap-datetimepicker.js
+++ vendor/assets/javascripts/bootstrap-datetimepicker.js
@@ -63,20 +63,6 @@
         if (!this.format) this.format = 'MM/dd/yyyy';
       }
       this._compileFormat();
-      if (this.component) {
-        icon = this.component.find('i');
-      }
-      if (this.pickTime) {
-        if (icon && icon.length) this.timeIcon = icon.data('time-icon');
-        if (!this.timeIcon) this.timeIcon = 'icon-time';
-        icon.addClass(this.timeIcon);
-      }
-      if (this.pickDate) {
-        if (icon && icon.length) this.dateIcon = icon.data('date-icon');
-        if (!this.dateIcon) this.dateIcon = 'icon-calendar';
-        icon.removeClass(this.timeIcon);
-        icon.addClass(this.dateIcon);
-      }
       this.widget = $(getTemplate(this.timeIcon, options.pickDate, options.pickTime, options.pick12HourFormat, options.pickSeconds, options.collapse)).appendTo('body');
       this.minViewMode = options.minViewMode||this.$element.data('date-minviewmode')||0;
       if (typeof this.minViewMode === 'string') {
